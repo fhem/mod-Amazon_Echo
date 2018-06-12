5a864a8e20ead2c7fbf2caae974e9a3232fada2b
Christoph Morrison <post@christoph-jeschke.de>
2018-06-12 20:14:57 +0200

    Beautified the generated CHANGED output for no reason

--- prepare_controlfile.sh
+++ prepare_controlfile.sh
@@ -5,6 +5,8 @@ source $HOME/.profile
 controls_file="controls_echodevice.txt"
 changed_file="CHANGED"
 
+current_version=$(perl -0777 -ne 'print "$1" if /\"(\d+\.\d+\.\d+)\"\;/' FHEM/37_echodevice.pm)
+
 find . -type f -iname '.DS_Store' -delete
 
 rm ${controls_file}
@@ -16,6 +18,8 @@ do
 done
 
 rm ${changed_file}
-echo "*** And in this weeks episode at $(basename `git rev-parse --show-toplevel`):" >> ${changed_file}
-git log HEAD --pretty="%h %ad %s" --date=format:"%d.%m.%Y %H:%M" FHEM/ www/ >> ${changed_file}
+echo "Current version: ${current_version} from $(date +%d.%m.%Y)" > ${changed_file}
+echo "" >> ${changed_file}
+echo "And in the last weeks episode at $(basename `git rev-parse --show-toplevel`):" >> ${changed_file}
+git log HEAD --pretty="  %h %ad %s" --date=format:"%d.%m.%Y %H:%M" FHEM/ www/ >> ${changed_file}
 
